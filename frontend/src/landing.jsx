bb8b22d7aba83bdead953f30c1e0e9cdd1da811d
3kdev <ekdev@me.com>
2023-11-27 14:30:31 -0600

    added search bar + landing page elements

--- frontend/src/landing.jsx
+++ frontend/src/landing.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import searchImage from "./assets/search.png";
 import "./landing.css";
+
 
 function Landing() {
   const [count, setCount] = useState(0);
 
   return (
+    // Source: https://www.youtube.com/watch?v=9hnJsNIBq1g
     <>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto placeat, voluptatum libero earum sequi nam voluptatibus aspernatur. Tempore, labore reiciendis, porro quibusdam illo libero quia quas veniam hic est cupiditate?</p>
+    <div className="title">
+        <h1>AggieSpiritSips</h1>
+    </div>
+    <div className="search-container">
+        <form action="">
+            <input type="text" placeholder="enter a keyword"/>
+            <button type="submit">
+                <img src={searchImage}/>
+            </button>
+        </form>
+    </div>
     </>
   );
 }
