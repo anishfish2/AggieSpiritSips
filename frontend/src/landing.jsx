e006ded06da8218978db9e2a83831fbfe1af239f
3kdev <ekdev@me.com>
2023-11-27 13:08:21 -0600

    created landing page and landing css

---
+++ frontend/src/landing.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./landing.css";
+
+function Landing() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto placeat, voluptatum libero earum sequi nam voluptatibus aspernatur. Tempore, labore reiciendis, porro quibusdam illo libero quia quas veniam hic est cupiditate?</p>
+    </>
+  );
+}
+
+export default Landing;
